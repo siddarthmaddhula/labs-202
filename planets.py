604bdc26e39a393eb6225c34dfb671da5f65cf38
Siddarth Maddhula <siddarthmaddhula@Siddarths-MacBook-Pro.local>
2022-09-19 09:55:45 -0700

    initial commit

--- planets.py
+++ planets.py
@@ -1,10 +1,17 @@
 # Given earth weight and planet, returns weight on provided planet
 # If string does not match a planet, raise ValueError
 def weight_on_planets(pounds: float, planet: str) -> float:
-   # write your code here
-   return 0.0
+    if planet == 'Mars':
+        return pounds * 0.38
+    if planet == 'Jupiter':
+        return pounds * 2.34
+    if planet == 'Venus':
+        return pounds * 0.91
+    raise ValueError
+    return 0.0
 
-if __name__ == '__main__':    # pragma: no cover
-   pounds = float(input("What do you weigh on earth? "))
-   print("\nOn Mars you would weigh", weight_on_planets(pounds, 'Mars'), "pounds.\n" +
+
+if __name__ == '__main__':  # pragma: no cover
+    pounds = float(input("What do you weigh on earth? "))
+    print("\nOn Mars you would weigh", weight_on_planets(pounds, 'Mars'), "pounds.\n" +
           "On Jupiter you would weigh", weight_on_planets(pounds, 'Jupiter'), "pounds.")
--- planets_tests.py
+++ planets_tests.py
@@ -9,6 +9,14 @@ class Test_planets(unittest.TestCase):
         weight = 136
         self.assertAlmostEqual(weight_on_planets(weight,"Mars"),51.68)
 
+    def test_jupiter(self) -> None:
+        weight = 136
+        self.assertAlmostEqual(weight_on_planets(weight,"Jupiter"),318.24)
+
+    def test_venus(self) -> None:
+        weight = 136
+        self.assertAlmostEqual(weight_on_planets(weight,"Venus"),123.76)
+
     def test_error(self) -> None:
         with self.assertRaises(ValueError):  # uses context manager for checking exception
             weight = 99
